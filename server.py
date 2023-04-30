32b07e16eed6d89bb289b145515aa1f067e64ba6
ChanRithy27 <chanrithy27@icloud.com>
2023-04-30 00:30:09 -0700

    Added different routes as wel as updated my templates.

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, redirect, session, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+app.secret_key = "this is a key to the city"
+
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/survey', methods=['POST'])
+def survey_info():
+    session['username'] = request.form['name']
+    session['location'] = request.form['dojoLocation']
+    session[''] = request.form['']
+    session[''] = request.form['']
+    return redirect('/results')
+
+@app.route('/results')
+def display_results():
+    return render_template('results.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
